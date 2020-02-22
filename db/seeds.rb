cacf009fde833c4b83b5d4751cd46189858f6ec1
averagealloy <m.mctighe10@gmail.com>
2020-02-22 18:42:51 -0500

    adding vals back in and debugging then uncommenting the second trans act in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ account = Account.create(name: 'that smhoney spot', balance: 100000)
 
 transaction = Transaction.create(account_id: 1 , amount: 10000, kind: "deposit", date: Date.today, about:'get paidizzled')
 
-# transaction_two = Transaction.create(account_id: 1 , amount: 10000, type: 'idk man', date: Date.today, about:'gettin unpaidizzled')
+transaction_two = Transaction.create(account_id: 1 , amount: 10000, kind: "withdraw", date: Date.today, about:'gettin unpaidizzled')
