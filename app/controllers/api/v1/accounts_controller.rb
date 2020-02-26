ddefc5770b50c9f060b37442008251c821fc25b2
averagealloy <m.mctighe10@gmail.com>
2020-02-26 16:18:41 -0500

    removed instance var or '@' from accoints controller tried the same in transactions controller but it breaks

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::AccountsController < ApplicationController
 
     def index
-        @accounts = Account.all 
-        render json: @accounts 
+        accounts = Account.all 
+        render json: accounts 
     end 
 
     def create
         
-        @account = Account.new(account_params)
-        if @account.save
-            render json: @account
+        account = Account.new(account_params)
+        if account.save
+            render json: account
         else
             render json: {error: 'error creating account'}
         end 
     end 
 
     def show
-        @account = Account.find(params[:id])
-        render json: @account
+        account = Account.find(params[:id])
+        render json: account
     end 
 
     def destroy
-        @account = Account.find(params[:id])
-        @account.destroy
+        account = Account.find(params[:id])
+        account.destroy
     end 
 
     def update
-        @account = Account.find(params[:id])
-        @account.update(name: params["account"]["name"])
-        @account.save
-        render json: @account
+        account = Account.find(params[:id])
+        account.update(name: params["account"]["name"])
+        account.save
+        render json: account
     end
 
     private
